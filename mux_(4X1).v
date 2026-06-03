bff96281126168a6d775c7c75310d68a45b105d6
prakhar6677 <prakharverma397@gmail.com>
2026-06-03 15:28:05 +0530

    Added mux (4X1) design

---
+++ mux_(4X1).v
@@ -0,0 +1,19 @@
+module mux4x1(
+    input I0,
+    input I1,
+    input I2,
+    input I3,
+    input [1:0] S,
+    output reg Y
+);
+
+always @(*) begin
+    case(S)
+        2'b00: Y = I0;
+        2'b01: Y = I1;
+        2'b10: Y = I2;
+        2'b11: Y = I3;
+    endcase
+end
+
+endmodule
