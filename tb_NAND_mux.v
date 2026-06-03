1821f2425af2cef1e98bec3dafa883c58a02e7d3
prakhar6677 <prakharverma397@gmail.com>
2026-06-03 16:01:22 +0530

    Added NAND gate using 2X1 mux testbench

---
+++ tb_NAND_mux.v
@@ -0,0 +1,28 @@
+`timescale 1ns/1ps
+
+module tb;
+
+reg A, B;
+wire Y;
+
+nand_using_mux uut (
+    .A(A),
+    .B(B),
+    .Y(Y)
+);
+
+initial begin
+
+    $dumpfile("dump.vcd");
+    $dumpvars(0, tb);
+
+    A = 0; B = 0; #10;
+    A = 0; B = 1; #10;
+    A = 1; B = 0; #10;
+    A = 1; B = 1; #10;
+
+    #10 $finish;
+
+end
+
+endmodule
