9b400e7a1ece6950af05f9ad3320628fda230439
prakhar6677 <prakharverma397@gmail.com>
2026-06-08 11:32:08 +0530

    Added 1XN demux design

---
+++ demux_1XN.v
@@ -0,0 +1,12 @@
+module demux #(parameter N = 8)(
+    input in,
+    input [$clog2(N)-1:0] sel,
+    output reg [N-1:0] out
+);
+
+always @(*) begin
+    out = 0;
+    out[sel] = in;
+end
+
+endmodule
